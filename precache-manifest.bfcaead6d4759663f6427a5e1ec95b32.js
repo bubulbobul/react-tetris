039d415868d8f0c0ce653739b420088d8a4df56c
bubulbobul <windam@live.fr>
2019-08-16 21:42:55 +0530

    Updates

---
+++ precache-manifest.bfcaead6d4759663f6427a5e1ec95b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d392592c78cda8678fbe90593a8ae679",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "334fbdd961937c1f58a4",
+    "url": "/react-tetris/static/css/main.eba53144.chunk.css"
+  },
+  {
+    "revision": "60d8e70fda9dcfef9ce5",
+    "url": "/react-tetris/static/js/2.80244021.chunk.js"
+  },
+  {
+    "revision": "334fbdd961937c1f58a4",
+    "url": "/react-tetris/static/js/main.4d9edcf8.chunk.js"
+  },
+  {
+    "revision": "3cbf365fbc4eeca393c0",
+    "url": "/react-tetris/static/js/runtime~main.1a1357f3.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "8641b8928b5319b5c2561864ad315612",
+    "url": "/react-tetris/static/media/giphy.8641b892.gif"
+  }
+]);
